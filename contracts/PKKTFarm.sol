42aa46b70b36c41bfce839e74f4bec7f5fc8c8e2
Matt Auer <mattauer@umich.edu>
2021-12-04 15:40:02 -0500

    refactor: remove unnecessary import

--- contracts/PKKTFarm.sol
+++ contracts/PKKTFarm.sol
@@ -3,7 +3,6 @@ pragma solidity =0.8.4;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-//import {SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Pool} from "./libraries/Pool.sol"; 
