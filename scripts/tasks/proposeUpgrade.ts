abe8dd5f4ee83c9e4cc25f6f5d6c4e3f595388e9
Matt Auer <mattauer@umich.edu>
2021-12-09 21:31:05 -0500

    chore add tasks to propose and upgrade contracts

---
+++ scripts/tasks/proposeUpgrade.ts
@@ -0,0 +1,23 @@
+
+const main = async ({ proxyname, implname, libraryname }, { deployments, getNamedAccounts }) => {
+    const { deploy, catchUnknownSigner } = deployments;
+    const { deployer, owner } = await getNamedAccounts();
+    const library = await deployments.get(libraryname);
+    // Upgrade the proxied implementation
+    console.log("Deploying new implementation and proposing upgrade...");
+    await catchUnknownSigner(
+        deploy(proxyname, {
+            from: deployer,
+            contract: implname,
+            proxy: {
+              owner: owner,
+              proxyContract: "OpenZeppelinTransparentProxy",
+            },
+            libraries: {
+              [libraryname]: library.address,
+            },
+        })
+    );
+}
+
+export default main;
