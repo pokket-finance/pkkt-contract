c222d71b9ee16b3747ceec265bf672a839230d3c
Matt Auer <mattauer@umich.edu>
2021-12-27 09:10:02 -0500

    chore: streamline indexing process

--- scripts/tasks/backend/generateSubgraphManifest.ts
+++ scripts/tasks/backend/generateSubgraphManifest.ts
@@ -5,9 +5,11 @@ import YAML from "yaml";
  * Writes data to the subgraph manifest
  * Requires the name in subgraph manifest for contracts is the same as the deployment name
  * @param startBlock the block to start indexing data from, if not specified it defaults to 13,600,000 
+ * @param network the network we are deploying the graph to
  * @param deployments gives us access to deployed smart contracts
  */
-const main = async ({ startblock = 13600000 }, { deployments }) => {
+const main = async ({ startblock = 13600000 }, { network, deployments }) => {
+    const networkName = network.name === "rinkeby" ? network.name : "mainnet";
     const subgraphManifestFile = "./subgraph.yaml";
     const subgraphManifest = fs.readFileSync(subgraphManifestFile, "utf-8");
     let parsedSubgraph = YAML.parse(subgraphManifest);
@@ -18,6 +20,7 @@ const main = async ({ startblock = 13600000 }, { deployments }) => {
         PKKTHodlBoosterOption = await deployments.get(optionName);
         dataSource.source.address = PKKTHodlBoosterOption.address;
         dataSource.source.startBlock = startblock;
+        dataSource.network = networkName;
     }
     let stringSubgraph = YAML.stringify(parsedSubgraph);
     fs.writeFileSync(subgraphManifestFile, stringSubgraph, "utf-8");
