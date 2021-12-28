28103e2d70cc6fb1481e137f5b4b2bce9c93e883
Matt Auer <mattauer@umich.edu>
2021-12-28 15:39:07 -0500

    chore: add route to show option tvl data

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import express from "express";
 import path from "path";
 import { ethers } from "hardhat";
-import { Signer } from "ethers";
+import { BigNumber, BigNumberish, Signer } from "ethers";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -27,12 +27,106 @@ const port = 3000;
 // config
 // Decode Form URL encoded data
 app.use(express.urlencoded());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "/views"));
 
 app.get("/initiateEpoch", async (req, res) => {
     res.sendFile(path.join(__dirname, "/views/initiateEpoch.html"));
 });
 
 app.post("/initiateEpoch", async (req, res) => {
+    const [
+        optionVault,
+        ethHodlBoosterCallOption,
+        ethHodlBoosterPutOption,
+        wbtcHodlBoosterCallOption,
+        wbtcHodlBoosterPutOption
+    ] = await getOptionContracts();
+    const ethCallPremium = parseFloat(req.body.ethCallPremium);
+    const ethPutPremium = parseFloat(req.body.ethPutPremium);
+    const wbtcCallPremium = parseFloat(req.body.wbtcCallPremium);
+    const wbtcPutPremium = parseFloat(req.body.wbtcPutPremium);
+    const ethCallStrikePrice = req.body.ethCallStrike * (10 ** ETH_PRICE_PRECISION);
+    const ethPutStrikePrice = req.body.ethPutStrike * (10 ** ETH_PRICE_PRECISION);
+    const wbtcCallStrikePrice = req.body.wbtcCallStrike * (10 ** ETH_PRICE_PRECISION);
+    const wbtcPutStrikePrice = req.body.wbtcPutSrike * (10 ** ETH_PRICE_PRECISION);
+    let optionParameters = [
+        {
+            pricePrecision: ETH_PRICE_PRECISION,
+            strikePrice: ethCallStrikePrice,
+            premiumRate: ethCallPremium * RATIO_MULTIPLIER,
+            option: ethHodlBoosterCallOption.address
+        },
+        {
+            pricePrecision: ETH_PRICE_PRECISION,
+            strikePrice: ethPutStrikePrice,
+            premiumRate: ethPutPremium * RATIO_MULTIPLIER,
+            option: ethHodlBoosterPutOption.address
+        },
+        {
+            pricePrecision: WBTC_PRICE_PRECISION,
+            strikePrice: wbtcCallStrikePrice,
+            premiumRate: wbtcCallPremium * RATIO_MULTIPLIER,
+            option: wbtcHodlBoosterCallOption.address
+        },
+        {
+            pricePrecision: WBTC_PRICE_PRECISION,
+            strikePrice: wbtcPutStrikePrice,
+            premiumRate: wbtcPutPremium * RATIO_MULTIPLIER,
+            option: wbtcHodlBoosterPutOption.address
+        }
+    ];
+    const [, settler] = await ethers.getSigners();
+    await optionVault.connect(settler as Signer).setOptionParameters(optionParameters);
+    res.send(JSON.stringify(req.body));
+});
+
+app.get("/", async (req, res) => {
+    const [
+        optionVault,
+        ethHodlBoosterCallOption,
+        ethHodlBoosterPutOption,
+        wbtcHodlBoosterCallOption,
+        wbtcHodlBoosterPutOption
+    ] = await getOptionContracts();
+    const options = [
+        ethHodlBoosterCallOption,
+        ethHodlBoosterPutOption,
+        wbtcHodlBoosterCallOption,
+        wbtcHodlBoosterPutOption
+    ]
+    const optionTVLData = await getTVLOptionData(options, optionVault);
+    // let optionDataTable = document.getElementById("optionTVLData") as HTMLElement;
+    // for(let option of optionTVLData) {
+    //     let newRow = document.createElement("tr");
+    //     let nameData = document.createElement("td");
+    //     nameData.innerHTML = option.name;
+    //     let activeData = document.createElement("td");
+    //     activeData.innerHTML = option.active;
+    //     let lockedData = document.createElement("td");
+    //     lockedData.innerHTML = option.locked;
+    //     let releasedData = document.createElement("td");
+    //     releasedData.innerHTML = option.released;
+    //     let releasedCounterPartyData = document.createElement("td");
+    //     releasedCounterPartyData.innerHTML = option.releasedCounterParty;
+    //     newRow.appendChild(nameData); 
+    //     newRow.appendChild(activeData);
+    //     newRow.appendChild(lockedData);
+    //     newRow.appendChild(releasedData);
+    //     newRow.appendChild(releasedCounterPartyData);
+    //     optionDataTable.appendChild(newRow);   
+    // }
+    res.render("optionTVLData", { optionTVLData });
+});
+
+async function getOptionContracts(): Promise<[
+    OptionVault,
+    PKKTHodlBoosterOption,
+    PKKTHodlBoosterOption,
+    PKKTHodlBoosterOption,
+    PKKTHodlBoosterOption
+]> {
+    const optionVault = await getDeployedContractHelper("OptionVault") as OptionVault;
     const ethHodlBoosterCallOption = await getDeployedContractHelper(
         "ETHHodlBoosterCallOption"
     ) as PKKTHodlBoosterOption;
@@ -45,41 +139,14 @@ app.post("/initiateEpoch", async (req, res) => {
     const wbtcHodlBoosterPutOption = await getDeployedContractHelper(
         "WBTCHodlBoosterPutOption"
     ) as PKKTHodlBoosterOption;
-    const ethCallPremium = parseFloat(req.body.ethCallPremium);
-    const ethPutPremium = parseFloat(req.body.ethPutPremium);
-    const wbtcCallPremium = parseFloat(req.body.wbtcCallPremium);
-    const wbtcPutPremium = parseFloat(req.body.wbtcPutPremium);
-    let optionParameters = [
-        {
-            pricePrecision: ETH_PRICE_PRECISION,
-            strikePrice: req.body.ethCallStrike,
-            premiumRate: ethCallPremium * RATIO_MULTIPLIER,
-            option: ethHodlBoosterCallOption.address
-        },
-        {
-            pricePrecision: ETH_PRICE_PRECISION,
-            strikePrice: req.body.ethPutStrike,
-            premiumRate: ethPutPremium * RATIO_MULTIPLIER,
-            option: ethHodlBoosterPutOption.address
-        },
-        {
-            pricePrecision: WBTC_PRICE_PRECISION,
-            strikePrice: req.body.wbtcCallStrike,
-            premiumRate: wbtcCallPremium * RATIO_MULTIPLIER,
-            option: wbtcHodlBoosterCallOption.address
-        },
-        {
-            pricePrecision: WBTC_PRICE_PRECISION,
-            strikePrice: req.body.wbtcPutStrike,
-            premiumRate: wbtcPutPremium * RATIO_MULTIPLIER,
-            option: wbtcHodlBoosterPutOption.address
-        }
+    return [
+        optionVault,
+        ethHodlBoosterCallOption,
+        ethHodlBoosterPutOption,
+        wbtcHodlBoosterCallOption,
+        wbtcHodlBoosterPutOption
     ];
-    const [, settler] = await ethers.getSigners();
-    const optionVault = await getDeployedContractHelper("OptionVault") as OptionVault;
-    await optionVault.connect(settler as Signer).setOptionParameters(optionParameters);
-    res.send(JSON.stringify(req.body));
-});
+}
 
 // app.get("/graph", async (req, res) => {
 //     const url = "https://api.thegraph.com/subgraphs/name/matt-user/option-rinkeby";
@@ -113,38 +180,3 @@ app.post("/initiateEpoch", async (req, res) => {
 app.listen(port, () => {
     console.log(`server is listening on ${port}`);
 });
-
-// Makes api request to etherscan to get data about our options
-const getEtherScanData = async (hodlBoosterOption) => {
-    console.log(`Retrieving Data from Etherscan for ${hodlBoosterOption.address}`);
-    const response = await getData(
-        {
-            module: "account",
-            action: "tokentx",
-            address: hodlBoosterOption.address,
-            startblock: "0",
-            endblock: "99999999",
-            page: "1",
-            offset: "100",
-            sort: "asc"
-        }
-    );
-    const result = response.data.result;
-    const blockNumbers = result.map(res => res.blockNumber);
-    console.log(blockNumbers);
-    for(let blockNumber of blockNumbers) {
-        const optionState = await hodlBoosterOption.getRoundData(blockNumber);
-        //printOptionState(optionState);
-    }
-}
-
-// Helper function to generate api url and request the endpoint
-const getData = async (params) => {
-    let url = "https://api-rinkeby.etherscan.io/api?";
-    // Generate api url parameters
-    url += Object.entries(params).map(([key, value]) => `${key}=${value}`).join("&");
-    url += `&apikey=${[process.env.ETHERSCAN_API_KEY]}`;
-    return axios.get(url);
-}
-
-//main();
