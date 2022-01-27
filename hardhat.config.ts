36c2ce7e7917aea32f6486029985429e283c923a
adacai1982 <adacai1982@hotmail.com>
2022-01-27 13:45:48 +0800

    update for qa deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -52,7 +52,7 @@ var accounts = data.deployerPrivateKey ?
       accounts: accounts,
     },
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: process.env.ROPSTEN_RPC_URL ?? `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       gas: 2100000, 
       accounts: accounts,
       gasPrice: 20e9,
